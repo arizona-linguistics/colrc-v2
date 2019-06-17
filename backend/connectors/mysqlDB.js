16a8fcbd7d69907770cd9b3c4ec3749196219cd6
Gus Hahn-Powell <gushahnpowell@gmail.com>
2019-06-17 14:00:43 -0700

    Cleanup
    - Removed .gitkeep placeholder
    - Default "mysql" for DB dialect

--- backend/connectors/mysqlDB.js
+++ backend/connectors/mysqlDB.js
@@ -10,7 +10,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    dialect: (process.env.DB_DIALECT || "mysql"),
     //operatorsAliases: false,
     pool: { max: 5, min: 0, acquire: 30000, idle: 10000 },
     define: {
