4b520c47105e38d8d9f76986a3ff727523e3a4c3
amyfou <36521868+amyfou@users.noreply.github.com>
2019-07-19 09:58:07 -0700

    backend handles users.roles as array in updateAdminMutation

--- frontend/src/Colrc.js
+++ frontend/src/Colrc.js
@@ -36,6 +36,7 @@ import ChangePassword from './users/ChangePassword';
 import UserProfile from './users/UserProfile';
 import Register from './users/Register';
 import Admin from './users/Admin';
+import UpdateRoles from './users/UpdateRoles';
 import Settings from './settings/Settings';
 import More from './more/More';
 import { createHttpLink } from 'apollo-link-http';
@@ -162,6 +163,7 @@ class Colrc extends Component {
                 <Route path="/userprofile" component={UserProfile} />
                 <Route path="/register" component={() => <Register changeLoginState={this.changeLoginState} />} />
                 <Route path="/admin" component={Admin} />
+                <Route path="/updateroles" component={UpdateRoles} />                
                 <Route path="/Settings" component={Settings}  />
                 <Route path="/more" component={More}  />
                 {/* <Route component={NotFound} /> */}
