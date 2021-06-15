333664ddba636a8013c163bf008972b192103a1c
jivens <john.wagner.ivens@gmail.com>
2021-06-15 16:18:29 -0700

    Really fixed the entrypoint.sh to ignore colrc-files.sql

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -28,7 +28,7 @@ create_html_dir() {
     mkdir -p ${NGINX_HTML_DIR}
     chmod -R 755 ${NGINX_HTML_DIR}
     echo "<html><head><title>My First HTML Page</title></head><body>My text goes here.</body></html>" > ${NGINX_HTML_DIR}/index.html
-    chmod -R 755 ${NGINX_FILE_DIR}
+    #chmod -R 755 ${NGINX_FILE_DIR}
     chmod 777 ${NGINX_HTML_DIR}/index.html
 }
 
