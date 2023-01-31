2f56d363072c6b172ec39299b31464c023dd6c00
Amy Fountain <a.v.fountain@gmail.com>
2023-01-31 16:01:15 -0700

    cleanup for students and update footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
                 <Grid.Column>
                     <Grid.Row>
                         <Message compact size="mini">
-                            <p>Coeur d'Alene online language resource center copyright 2009-2021, rights to language content reserved by the Coeur d'Alene Tribal Language Programs.</p>
+                            <p>Coeur d'Alene online language resource center copyright 2009-2023, rights to language content reserved by the Coeur d'Alene Tribal Language Programs.</p>
                             <p>Project supported by the National Science Foundation awards BCS-1160627 and BCS-1160394 and the National Endowment for the Humanities award PD-261031-18.</p>
                         </Message>
                     </Grid.Row>
