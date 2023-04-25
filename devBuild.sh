ed29de4052b06054ab136ade03bb7c3b7f67f1cb
Amy Fountain <a.v.fountain@gmail.com>
2023-04-25 14:30:27 -0700

    texts and audios hidden by default

--- devBuild.sh
+++ devBuild.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 #build and up the development environment
-docker compose -f docker-compose.yml -f docker-compose.override.yml up --build
+docker-compose -f docker-compose.yml -f docker-compose.override.yml up --build
 
 #up the development envioronment without building
 #docker compose -f docker-compose.yml -f docker-compose.override.yml up
-
